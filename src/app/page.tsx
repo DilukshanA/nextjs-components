e20aca089122451b84bfdee0e914545549507fa2
Dilukshan <dilukshan2020da@gmail.com>
2025-04-09 22:22:29 +0530

    seperate components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,14 @@
-import { ImagesSliderDemo } from "@/components/ImagesSliderDemo";
+"use client";
 import { ImagesSliderDemo2 } from "@/components/ImagesSliderDemo2";
 import { ImagesSliderDemo3 } from "@/components/ImagesSliderDemo3";
-import { TailwindcssButtons } from "@/components/TailwindcssButtons";
-
-import Image from "next/image";
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
+
+  const router = useRouter();
+
   return (
     <div>
-      <div>
-        <button>Image Slider - 1</button>
-      </div>
-      Next js components
-      <ImagesSliderDemo/>
       <div className="my-10">
         <h1>Images Slider 2</h1>
       </div>
@@ -21,8 +17,15 @@ export default function Home() {
         <h1>Images Slider 3</h1>
       </div>
       <ImagesSliderDemo3/>
-      <div>
-        <TailwindcssButtons/>
+
+      <div className="flex justify-center items-center">
+      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        onClick={() => router.push('/images-slider-1')}>
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          Image Slider - 1
+        </span>
+      </button>
       </div>
     </div>
   );
