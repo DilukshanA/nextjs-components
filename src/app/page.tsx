b9550f1769196f5f8a3cb4b0fd38bbee4a2daacf
Dilukshan <dilukshan2020da@gmail.com>
2025-04-10 23:30:48 +0530

    change main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { ImagesSliderDemo2 } from "@/components/ImagesSliderDemo2";
-import { ImagesSliderDemo3 } from "@/components/ImagesSliderDemo3";
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
@@ -8,8 +6,8 @@ export default function Home() {
   const router = useRouter();
 
   return (
-    <div>
-      <div className="flex justify-center items-center">
+    <div className='flex justify-center items-center h-screen'>
+      <div className="flex flex-col justify-center items-center gap-y-6">
       <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
         onClick={() => router.push('/images-slider-1')}>
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
@@ -22,6 +20,13 @@ export default function Home() {
         <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
         <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
           Image Slider - 2
+        </span>
+      </button>
+      <button className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+        onClick={() => router.push('/images-slider-3')}>
+        <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
+        <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-3 py-1 text-sm font-medium text-white backdrop-blur-3xl">
+          Image Slider - 3
         </span>
       </button>
       </div>
